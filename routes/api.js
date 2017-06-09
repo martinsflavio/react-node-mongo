f2a546ecd7a6dc924f8adf0091e6ae87a4b65836
martinsflavio <flaviodosreismartins@gmail.com>
2017-06-09 00:02:27 -0700

    routing post requests

--- routes/api.js
+++ routes/api.js
@@ -12,44 +12,70 @@ router.get('/:resource', (req, res, next) => {
   // check for invalid resource request
   if (controller === undefined) {
     // TODO CHECK IF IS OK THIS FORMAT
-    res.status(500).json({
+    return res.status(500).json({
       message: 'fail',
       err: `Invalid resource request : ${resource}`
     });
-  } else {
-    console.log('find');
-    controller.find(req.query, (err, results) => {
-      if (err) {
-        return res.status(500).json({message:'fail', err: err});
-      } else {
-        return res.status(200).json({message: 'success', body: results});
-      }
-    });
   }
+
+  controller.find(req.query, (err, results) => {
+    if (err) {
+      return res.status(500).json({message:'fail', err: err});
+    } else {
+      return res.status(200).json({message: 'success', body: results});
+    }
+  });
+
 });
 ///////////////////////////////////////////////////
 
 /* POST */
-router.post('/:resource/create', (req, res, next) => {
-  let resource = req.params.resource;
+router.post('/:resource/:method', (req, res, next) => {
+  let resource   = req.params.resource;
+  let method     = req.params.method;
   let controller = controllers[resource];
+
 
   // check for invalid resource request
   if (controller === undefined) {
     // TODO CHECK IF IS OK THIS FORMAT
-    res.status(500).json({
+    return res.status(500).json({
       message: 'fail',
       err: `Invalid resource request : ${resource}`
     });
-  } else {
-    controller.create(req.body, (err, results) => {
-      if (err) {
-        return res.status(500).json({message: 'fail', err: err});
-      } else {
-        return res.status(200).json({message: 'success', result: results});
-      }
+  } else if (controller[method] === undefined) {
+    return res.status(500).json({
+      message: 'fail',
+      err: `Invalid method request : ${method}`
     });
   }
+
+  switch (method) {
+    case 'create':
+      res.json({message: "create"});
+    break;
+    case 'find':
+      if (req.query.id === undefined) {
+        res.json({message:"find"});
+      } else {
+        res.json({message:"findById"});
+      }
+    break;
+    case 'update':
+      res.json({message:"update"});
+    break;
+    case 'destroy':
+      res.json({message:"destroy"});
+    break;
+  }
+/*  controller.create(req.body, (err, results) => {
+    if (err) {
+      return res.status(500).json({message: 'fail', err: err});
+    } else {
+      return res.status(200).json({message: 'success', result: results});
+    }
+  });*/
+
 });
 ///////////////////////////////////////////////////
 
